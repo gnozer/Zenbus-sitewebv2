9a358a7c7f7430ab4a59f7e1445b7c6d539f4534
Gno <simon.vergnaud@zenbus.fr>
2018-10-03 17:44:01 +0200

    [edit] passengers page (results tree)

--- assets/js/passengers.js
+++ assets/js/passengers.js
@@ -35,48 +35,135 @@ function sendDatasToSheet(email, lines, network, city) {
 	}
 }
 
+function compareSurveyCount(a,b) {
+  if (a.surveyCount > b.surveyCount)
+    return -1;
+  if (a.surveyCount < b.surveyCount)
+    return 1;
+  return 0;
+}
+
 /**
  * Function to init the survey results datas
  */
 function initSurveyResults() {
-	var accsWantedRoutes = [],
-		 citiesWithSurvey = [];
-	
-	for(var i = 0; i < API.accounts.length; i++ ) {
-		if(API.accounts[i].hasOwnProperty("wantedRoutes") && API.accounts[i].wantedRoutes.length != 0) {
-			accsWantedRoutes.push(API.accounts[i]);
+	return new Promise(function(resolve, reject){
+		var accsWantedRoutes = [],
+			 citiesWithSurvey = [];
+
+		for(var i = 0; i < API.accounts.length; i++ ) {
+			if(API.accounts[i].hasOwnProperty("wantedRoutes") && API.accounts[i].wantedRoutes.length != 0) {
+				accsWantedRoutes.push(API.accounts[i]);
+			}
 		}
-	}
-	var sameCity;
-	for(var i = 0; i < API.cities.length; i++ ) {
-		sameCity = [];
-		for(var j = 0; j < API.cities[i].namespaces.length; j++){
-			for(var k = 0; k < accsWantedRoutes.length; k++) {
-				if(API.cities[i].namespaces[j] == accsWantedRoutes[k].namespace){
-					if(sameCity.length == 0) {
-						citiesWithSurvey.push(API.cities[i]);
+		var sameCity;
+		for(var i = 0; i < API.cities.length; i++ ) {
+			sameCity = [];
+			for(var j = 0; j < API.cities[i].namespaces.length; j++){
+				for(var k = 0; k < accsWantedRoutes.length; k++) {
+					if(API.cities[i].namespaces[j] == accsWantedRoutes[k].namespace){
+						if(sameCity.length == 0) {
+							citiesWithSurvey.push(API.cities[i]);
+						}
+						sameCity.push(accsWantedRoutes[k]);
 					}
-					sameCity.push(accsWantedRoutes[k]);
 				}
 			}
+			if(sameCity.length != 0){
+				citiesWithSurvey[citiesWithSurvey.length-1].accounts = sameCity;
+			}
+
 		}
-		if(sameCity.length != 0){
-			citiesWithSurvey[citiesWithSurvey.length-1].accounts = sameCity;
+
+		for(var i = 0; i < citiesWithSurvey.length; i++){
+			citiesWithSurvey[i].surveyCount = 0;
+			for(var j = 0; j < citiesWithSurvey[i].accounts.length; j++){
+				citiesWithSurvey[i].accounts[j].surveyCount = 0;
+				for(var k = 0; k < citiesWithSurvey[i].accounts[j].wantedRoutes.length; k++){
+					citiesWithSurvey[i].accounts[j].surveyCount += citiesWithSurvey[i].accounts[j].wantedRoutes[k].surveyCount;
+					
+				}
+				citiesWithSurvey[i].accounts[j].wantedRoutes.sort(compareSurveyCount)
+				citiesWithSurvey[i].surveyCount += citiesWithSurvey[i].accounts[j].surveyCount;
+			}
+		}
+		resolve(citiesWithSurvey);
+	});
+}
+
+function buildSurveyResults(datas){
+	var resultsDiv = document.getElementById("resultsTree"),
+		 totalRequests = 0,
+		 totalCityRequests;
+	
+	for(var i = 0; i < datas.length; i++) {
+		totalRequests+= datas[i].surveyCount;
+	}
+	console.log(datas);
+	for(var i = 0; i < datas.length; i++) {
+		var cityDetails = document.createElement("details"),
+			 citySummary = document.createElement("summary"),
+			 citySummaryContent = document.createTextNode(datas[i].name),
+			 cityAside = document.createElement("aside"),
+			 cityAsideContent = document.createTextNode(datas[i].surveyCount+" demandes"),
+			 cityProgress = document.createElement("progress");
+		cityProgress.setAttribute("max", 100);
+		cityProgress.setAttribute("value", datas[i].surveyCount*100/totalRequests);
+		cityAside.appendChild(cityAsideContent);
+		citySummary.appendChild(citySummaryContent);
+		cityDetails.appendChild(citySummary);
+		cityDetails.appendChild(cityAside);
+		cityDetails.appendChild(cityProgress);
+		
+		for(var j = 0; j < datas[i].accounts.length; j++){
+			var accDetails = document.createElement("details"),
+				 accSummary = document.createElement("summary"),
+				 accSummaryContent = document.createTextNode(datas[i].accounts[j].name),
+				 accAside = document.createElement("aside"),
+				 accAsideContent = document.createTextNode(datas[i].accounts[j].surveyCount),
+				 accProgress = document.createElement("progress"),
+				 accUl = document.createElement("ul");
+				 
+			
+			if(datas[i].accounts[j].wantedRoutes.length > 3){
+				for(var k=0; k < 3; k++){
+					var accLi = document.createElement("li");
+					accLi.innerHTML = datas[i].accounts[j].wantedRoutes[k].name+' (<span class="caca">'+datas[i].accounts[j].wantedRoutes[k].surveyCount+'</span>)';
+					accUl.appendChild(accLi);
+				}
+				
+				
+				var accLastLi = document.createElement("li"),
+					 textNode = "Autres : ";
+				
+				for(var k = 3; k < datas[i].accounts[j].wantedRoutes.length; k++){
+					textNode += datas[i].accounts[j].wantedRoutes[k].name+" (<span class=\"caca\">"+datas[i].accounts[j].wantedRoutes[k].surveyCount+"</span>) - "
+				}
+				accLastLi.innerHTML = textNode.slice(0,-3);
+				accUl.appendChild(accLastLi);
+			} else {
+				for(var k = 0; k < datas[i].accounts[j].wantedRoutes.length; k++){
+					var accLi = document.createElement("li");
+					accLi.innerHTML = datas[i].accounts[j].wantedRoutes[k].name+' (<span class="caca">'+datas[i].accounts[j].wantedRoutes[k].surveyCount+'</span>)';
+					accUl.appendChild(accLi);
+				}
+			}
+			
+			accProgress.setAttribute("max", 100);
+			accProgress.setAttribute("value", datas[i].accounts[j].surveyCount*100/datas[i].surveyCount);
+			accAside.appendChild(accAsideContent);
+			accSummary.appendChild(accSummaryContent);
+			accDetails.appendChild(accSummary);
+			accDetails.appendChild(accAside);
+			accDetails.appendChild(accProgress);
+			accDetails.appendChild(accUl);
+			
+			cityDetails.appendChild(accDetails);
 		}
 		
+		resultsDiv.appendChild(cityDetails);
+		
 	}
-	
-	for(var i = 0; i < citiesWithSurvey.length; i++){
-		citiesWithSurvey[i].surveyCount = 0;
-		for(var j = 0; j < citiesWithSurvey[i].accounts.length; j++){
-			citiesWithSurvey[i].accounts[j].surveyCount = 0;
-			for(var k = 0; k < citiesWithSurvey[i].accounts[j].wantedRoutes.length; k++){
-				citiesWithSurvey[i].accounts[j].surveyCount += citiesWithSurvey[i].accounts[j].wantedRoutes[k].surveyCount;
-			}
-			citiesWithSurvey[i].surveyCount += citiesWithSurvey[i].accounts[j].surveyCount;
-		}
-	}
-	console.log(citiesWithSurvey);
 }
 
 (function(){
@@ -91,7 +178,9 @@ function initSurveyResults() {
 		sendDatasToSheet(email, lines, network, city);
 	});
 	
-	initSurveyResults();
+	initSurveyResults().then(function(response){
+		return buildSurveyResults(response);
+	})
 })();
 
 document.getElementById('surveyCheckboxLines').onchange = function() {
